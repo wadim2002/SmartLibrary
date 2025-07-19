ce110a7bf31ddc724d26e582d40470c782e377c9
Савитский Вадим <savitskijvadim@Mac-mini-Savitskij.local>
2025-07-19 22:05:09 +0300

    first commit

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Book {
+    // Поля класса
+    String Label;                    // Название книги
+    short PageCount;                 // Объем стараниц
+    String ISBN;                     // ISBN
+    byte Edition;                    // Издание
+    short YearPublication;           // Год издания
+}
